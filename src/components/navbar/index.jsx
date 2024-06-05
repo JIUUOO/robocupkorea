ee189185f5d59d4e496d5ddd867d630d42d28f6f
JIUUOO <nninjiuuoo@gmail.com>
2024-06-05 16:49:48 +0900

    modify navbar and home pages

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,3 +1,20 @@
+import { NavLink } from "react-router-dom";
+
+import rckaLogo from "../../assets/images/logo/robocup-korea-association.png";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <nav className="bg-white navbar sticky top-0">
+      <div className="mx-auto max-w-7xl">
+        <div className="relative flex h-16 justify-between items-center">
+          <NavLink
+            to={"/"}
+            className="relative inline-flex items-center justify-center p-2"
+          >
+            <img src={rckaLogo} className="h-14" alt="RCKA"></img>
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <div className="text-xl">home</div>;
+  return (
+    <div className="home">
+      <div className="home-media--wrapper">
+        <iframe
+          className="home-media--content"
+          src="https://www.youtube.com/embed/QB5dvHHnSMk?si=H8vYmersBfckFq5m&autoplay=0&mute=1&loop=1&controls=0&color=white&modestbranding=0&rel=0&playsinline=1&enablejsapi=18&playlist=QB5dvHHnSMk"
+          title="RoboCup RCAP Pyeongchang 2023"
+          allow="autoplay;"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
 }
