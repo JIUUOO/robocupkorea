3ba46f0b3be0758e0b12d4aceb6dc2add2d422fd
JIUUOO <nninjiuuoo@gmail.com>
2024-06-07 23:44:59 +0900

    feat: add draopdown appear animation

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className="footer">
       <div className="container mx-auto mt-20 mb-4">
-        <div className="grid sm:grid-cols-3 mx-auto w-60 sm:w-auto">
+        <div className="grid sm:grid-cols-3 mx-auto w-56 sm:w-auto">
           <img src={ministryLogo} className="h-10 lg:h-11 sm:mx-auto"></img>
           <img src={serviceLogo} className="h-10 lg:h-11 sm:mx-auto"></img>
           <img src={commissionLogo} className="h-10 lg:h-11 sm:mx-auto"></img>
