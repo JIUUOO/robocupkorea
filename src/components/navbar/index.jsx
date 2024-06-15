53add0eed20bf82ec34cf4bb05256f79e39d30b2
JIUUOO <nninjiuuoo@gmail.com>
2024-06-15 10:54:21 +0900

    todo: make a nested nav

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -5,21 +5,6 @@ import { GlobalContext } from "../../context";
 import rckaLogo from "../../assets/images/logo/robocup-korea-association.png";
 
 export default function Navbar() {
-  const { scrollY, pathname } = useContext(GlobalContext);
-  const navbarRef = useRef(null);
-
-  // hide navbar after 3 sec
-  //   useEffect(() => {
-  //     if (pathname === "/") {
-  //       navbarRef.current.style.top = "0px";
-  //       const initTimer = setTimeout(() => {
-  //         navbarRef.current.style.top = "-64px"; // navbar hidden
-  //       }, 3700);
-
-  //       return () => clearTimeout(initTimer);
-  //     }
-  //   }, [scrollY]);
-
   const [openDropdown, setOpenDropdown] = useState(null);
   const openMenu = (dropdown) => {
     setOpenDropdown(dropdown);
@@ -39,165 +24,170 @@ export default function Navbar() {
 
   //   console.log(location.pathname);
   return (
-    <nav className="navbar" ref={navbarRef}>
-      <div className="container">
-        <div className="flex h-16 justify-between items-center">
-          <NavLink
-            to={"/"}
-            className="inline-flex items-center justify-center"
-            onClick={closePanel}
-          >
-            <img src={rckaLogo} className="max-h-16 py-1" alt="RCKA"></img>
+    <nav className="navbar">
+      <div className="test_container">
+        <div className="navbar__logo-wrapper">
+          <NavLink to={"/"} onClick={closePanel}>
+            <img src={rckaLogo} className="navbar__logo" alt="rckaLogo"></img>
           </NavLink>
+        </div>
+        <div
+          className="h-12 flex justify-center items-center button-mobile text-2xl"
+          onClick={handleClick}
+        >
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+
+        <ul
+          className={
+            click
+              ? "menu__panel menu__panel-is-active"
+              : "menu__panel menu__panel-is-inactive"
+          }
+        >
           <div
-            className="h-12 flex justify-center items-center button-mobile text-2xl"
-            onClick={handleClick}
+            onMouseEnter={() => openMenu("rcka")}
+            onMouseLeave={() => openMenu(null)}
           >
-            <i className={click ? "fas fa-times" : "fas fa-bars"} />
-          </div>
-          <ul className={click ? "menu-panel--open" : "menu-panel--close"}>
-            {/* menu start */}
+            <div className="dropdown__header">한국로보컵협회</div>
             <div
-              onMouseEnter={() => openMenu("association")}
-              onMouseLeave={() => openMenu(null)}
+              className={
+                openDropdown === "rcka"
+                  ? "dropdown-is-active"
+                  : "dropdown-is-inactive"
+              }
             >
-              <div className="dropdown--title">한국로보컵협회</div>
-              <div
-                className={
-                  openDropdown === "association"
-                    ? "dropdown--menu-open"
-                    : "dropdown--menu-close"
-                }
+              <NavLink
+                to={"/association/about"}
+                className="dropdown__items"
+                onClick={closeAll}
               >
-                <NavLink
-                  to={"/association/about"}
-                  className="dropdown--list"
-                  onClick={closeAll}
-                >
-                  소개
-                </NavLink>
-                <NavLink
-                  to={"/association/committee"}
-                  className="dropdown--list"
-                  onClick={closeAll}
-                >
-                  운영위원
-                </NavLink>
-              </div>
+                소개
+              </NavLink>
+              <NavLink
+                to={"/association/committee"}
+                className="dropdown__items"
+                onClick={closeAll}
+              >
+                운영위원
+              </NavLink>
             </div>
+          </div>
+          <div
+            onMouseEnter={() => openMenu("leagues")}
+            onMouseLeave={() => openMenu(null)}
+          >
+            <div className="dropdown__header">로보컵 리그</div>
             <div
-              onMouseEnter={() => openMenu("leagues")}
-              onMouseLeave={() => openMenu(null)}
+              className={
+                openDropdown === "leagues"
+                  ? "dropdown-is-active"
+                  : "dropdown-is-inactive"
+              }
             >
-              <div className="dropdown--title">로보컵 리그</div>
-              <div
-                className={
-                  openDropdown === "leagues"
-                    ? "dropdown--menu-open"
-                    : "dropdown--menu-close"
-                }
-              >
+              <div className="">
+                <span>RoboCupSoccer</span>
                 <NavLink
                   to="/leagues/robocup-soccer"
-                  className="dropdown--list"
+                  className="dropdown__items"
                   onClick={closeAll}
                 >
-                  RoboCupSoccer
-                </NavLink>
-                <NavLink
-                  to="/leagues/robocup-rescue"
-                  className="dropdown--list"
-                  onClick={closeAll}
-                >
-                  RoboCupRescue
-                </NavLink>
-                <NavLink
-                  to="/leagues/robocup-athome"
-                  className="dropdown--list"
-                  onClick={closeAll}
-                >
-                  RoboCup@Home
-                </NavLink>
-                <NavLink
-                  to="/leagues/robocup-industrial"
-                  className="dropdown--list"
-                  onClick={closeAll}
-                >
-                  RoboCupIndustrial
-                </NavLink>
-                <NavLink
-                  to="/leagues/robocup-junior"
-                  className="dropdown--list"
-                  onClick={closeAll}
-                >
-                  RoboCupJunior
+                  1
                 </NavLink>
               </div>
-            </div>
-            <div
-              onMouseEnter={() => openMenu("event")}
-              onMouseLeave={() => openMenu(null)}
-            >
-              <div className="dropdown--title">대회</div>
-              <div
-                className={
-                  openDropdown === "event"
-                    ? "dropdown--menu-open"
-                    : "dropdown--menu-close"
-                }
-              >
-                <NavLink
-                  to="/leagues"
-                  className="dropdown--list"
-                  onClick={closeAll}
-                >
-                  창의코딩대회
-                </NavLink>
-                <NavLink
-                  to="/leagues"
-                  className="dropdown--list"
-                  onClick={closeAll}
-                >
-                  기록
-                </NavLink>
-              </div>
-            </div>
-            <div
-              onMouseEnter={() => openMenu("notice")}
-              onMouseLeave={() => openMenu(null)}
-            >
-              <div className="dropdown--title">공지사항</div>
-              <div
-                className={
-                  openDropdown === "notice"
-                    ? "dropdown--menu-open"
-                    : "dropdown--menu-close"
-                }
-              >
-                <div className="dropdown--list" onClick={closeAll}>
-                  대회 일정
-                </div>
-                <div className="dropdown--list" onClick={closeAll}>
-                  경기 규정
-                </div>
-              </div>
-            </div>
-            {/* <div className="inline-block">
               <NavLink
-                to={"/association"}
+                to="/leagues/robocup-rescue"
+                className="dropdown__items"
+                onClick={closeAll}
+              >
+                RoboCupRescue
+              </NavLink>
+              <NavLink
+                to="/leagues/robocup-athome"
+                className="dropdown__items"
+                onClick={closeAll}
+              >
+                RoboCup@Home
+              </NavLink>
+              <NavLink
+                to="/leagues/robocup-industrial"
+                className="dropdown__items"
+                onClick={closeAll}
+              >
+                RoboCupIndustrial
+              </NavLink>
+              <NavLink
+                to="/leagues/robocup-junior"
+                className="dropdown__items"
+                onClick={closeAll}
+              >
+                RoboCupJunior
+              </NavLink>
+            </div>
+          </div>
+          <div
+            onMouseEnter={() => openMenu("events")}
+            onMouseLeave={() => openMenu(null)}
+          >
+            <div className="dropdown__header">로보컵 이벤트</div>
+            <div
+              className={
+                openDropdown === "events"
+                  ? "dropdown-is-active"
+                  : "dropdown-is-inactive"
+              }
+            >
+              <NavLink
+                to="/leagues"
+                className="dropdown__items"
+                onClick={closeAll}
+              >
+                창의코딩대회
+              </NavLink>
+              <NavLink
+                to="/leagues"
+                className="dropdown__items"
+                onClick={closeAll}
+              >
+                기록
+              </NavLink>
+            </div>
+          </div>
+          <div
+            onMouseEnter={() => openMenu("notice")}
+            onMouseLeave={() => openMenu(null)}
+          >
+            <div className="dropdown__header">공지사항</div>
+            <div
+              className={
+                openDropdown === "notice"
+                  ? "dropdown-is-active"
+                  : "dropdown-is-inactive"
+              }
+            >
+              <div className="dropdown__items" onClick={closeAll}>
+                대회 일정
+              </div>
+              <div className="dropdown__items" onClick={closeAll}>
+                경기 규정
+              </div>
+            </div>
+          </div>
+          {/* <div className="inline-block">
+              <NavLink
+                to={"/assoc"}
                 className="relative inline-flex items-center justify-center p-2"
               >
                 <span>한국어</span>
               </NavLink>
               <NavLink
-                to={"/association"}
+                to={"/assoc"}
                 className="relative inline-flex items-center justify-center p-2"
               >
                 <span>ENG</span>
               </NavLink>
             </div> */}
-          </ul>
-        </div>
+        </ul>
       </div>
     </nav>
   );
